5d37bca4490c7c18104e6286020dca764206860c
Maksat Sagandykov <54390231+maksssat@users.noreply.github.com>
2022-06-14 18:50:40 +0600

    добавлен React.StrictMode

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <DateSelector />
-      <Calendar />
-      <Todo />
-    </div>
+    <React.StrictMode>
+      <div className="App">
+        <DateSelector />
+        <Calendar />
+        <Todo />
+      </div>
+    </React.StrictMode>
   );
 }
 
