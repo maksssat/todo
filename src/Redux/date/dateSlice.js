4709e89973abb4a80001f10847e3e5fb5a0e0b45
Maksat Sagandykov <54390231+maksssat@users.noreply.github.com>
2022-06-23 17:29:52 +0600

    изменены структура стора и редьюсеры store/date

--- src/Redux/date/dateSlice.js
+++ src/Redux/date/dateSlice.js
@@ -21,38 +21,56 @@ export const monthArr = [
 const dateSlice = createSlice({
   name: "date",
   initialState: {
-    month: today.getMonth(),
-    year: today.getFullYear(),
-    selectedDate: `${today.getDate()} ${monthArr[today.getMonth()]} ${today.getFullYear()}`,
+    selectedDay: today.getDate(),
+    selectedMonth: today.getMonth(),
+    selectedYear: today.getFullYear(),
   },
   reducers: {
     monthDecrement(state) {
-      if (state.month === 0) {
-        state.month = 11;
-        state.year = state.year - 1;
-      } else state.month = state.month - 1;
+      if (state.selectedMonth === 0) {
+        state.selectedMonth = 11;
+        state.selectedYear = state.selectedYear - 1;
+      } else state.selectedMonth = state.selectedMonth - 1;
     },
     monthIncrement(state) {
-      if (state.month === 11) {
-        state.month = 0;
-        state.year = state.year + 1;
+      if (state.selectedMonth === 11) {
+        state.selectedMonth = 0;
+        state.selectedYear = state.selectedYear + 1;
       } else {
-        state.month = state.month + 1;
+        state.selectedMonth = state.selectedMonth + 1;
       }
     },
     selectDate(state, action) {
-      state.selectedDate = action.payload;
+      const dateArr = action.payload.split(" ");
+      state.selectedDay = +dateArr[0];
+      state.selectedMonth = +dateArr[1];
+      state.selectedYear = +dateArr[2];
+    },
+    dayDecrement(state) {
+      const newSelectedDate = new Date(state.selectedYear, state.selectedMonth, state.selectedDay - 1);
+      state.selectedDay = newSelectedDate.getDate();
+      state.selectedMonth = newSelectedDate.getMonth();
+      state.selectedYear = newSelectedDate.getFullYear();
+    },
+    dayIncrement(state) {
+      const newSelectedDate = new Date(state.selectedYear, state.selectedMonth, state.selectedDay + 1);
+      state.selectedDay = newSelectedDate.getDate();
+      state.selectedMonth = newSelectedDate.getMonth();
+      state.selectedYear = newSelectedDate.getFullYear();
     },
   },
 });
 
 export default dateSlice.reducer;
 
-export const { monthDecrement, monthIncrement, selectDate } = dateSlice.actions;
+export const { monthDecrement, monthIncrement, selectDate, dayDecrement, dayIncrement } =
+  dateSlice.actions;
 
-export const selectMonth = (state) => state.date.month;
-export const selectYear = (state) => state.date.year;
-export const selectedDate = (state) => state.date.selectedDate;
+export const selectDay = ({ date }) => date.selectedDay;
+export const selectMonth = ({ date }) => date.selectedMonth;
+export const selectYear = ({ date }) => date.selectedYear;
+export const selectedDate = ({ date }) =>
+  `${date.selectedDay} ${monthArr[date.selectedMonth]} ${date.selectedYear}`;
 export const selectDaysArr = createSelector(
   [selectMonth, selectYear, (state, today) => today],
   (month, year, today) => getDaysToRender(month, year, today)
